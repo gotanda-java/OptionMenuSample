fe77fb596f80c8d7aab969b6b846fc2d759e920b
i0001 <il0venature1008@gmail.com>
2014-05-26 11:11:26 +0900

    電話機能追加

--- src/example/android/optionmenusample/OptionMenuSampleActivity.java
+++ src/example/android/optionmenusample/OptionMenuSampleActivity.java
@@ -1,18 +1,23 @@
 package example.android.optionmenusample;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 
-public class OptionMenuSampleActivity extends Activity {
+public class OptionMenuSampleActivity extends Activity implements LocationListener {
 
-	private static final int REQUEST_IMAGE_CAPTURE = 0;
+		private LocationManager mgr;
+		private static final int REQUEST_IMAGE_CAPTURE = 0;
 
 
 	@Override
@@ -37,8 +42,8 @@ public class OptionMenuSampleActivity extends Activity {
 		item3.setIcon(android.R.drawable.ic_menu_mylocation);
 		MenuItem item4 =menu.add(0,3,0,"Massage");
 		item4.setIcon(android.R.drawable.ic_dialog_email);
-		MenuItem item5 =menu.add(0,4,0,"Map");
-		item5.setIcon(android.R.drawable.ic_dialog_map);
+		MenuItem item5 =menu.add(0,4,0,"Call");
+		item5.setIcon(android.R.drawable.ic_menu_call);
 
 		//
 		SubMenu item6 =menu.addSubMenu(0,5,0,"Others");
@@ -55,7 +60,10 @@ public class OptionMenuSampleActivity extends Activity {
 		// onOptionsItemSelectedメソッド(メニューアイテム選択処理)
 		@Override
 		public boolean onOptionsItemSelected(MenuItem item) {
-			String action = null;
+
+			// インテントのインスタンス生成
+			Intent intent = new Intent();
+
 			switch (item.getItemId()) {
 				case 0:
 					showDialog("Saved !");
@@ -63,24 +71,40 @@ public class OptionMenuSampleActivity extends Activity {
 				case 1:
 					/*showDialog("Selected Camera !");*/
 					startCamera();
-
 					return true;
-				case 2:
 
+				case 2://Location
+					mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+					showDialog("location start!");
+					mgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
 					return true;
+
 				case 3:
-					// インテントのインスタンス生成
-					Intent intent = new Intent();
+
 					// インテントにアクション及び送信情報をセット
 					intent.setAction(Intent.ACTION_SENDTO);
 					intent.setData(Uri.parse("mailto:xxx@gmail.com"));
 					intent.putExtra(Intent.EXTRA_SUBJECT, "TEST");
 					intent.putExtra(Intent.EXTRA_TEXT, "これはテストメールです.");
-			// メール起動
-			startActivity(intent);
+					// メール起動
+					startActivity(intent);
 					return true;
-				case 4:
-					showDialog("Selected Map");
+				case 4://Call
+					//If you would like to display the screen you want to call,
+					//Please correct the sentence as follows.
+					//And delete the permission sentence from the Manifest file.
+					/*Intent intent = new Intent(
+						    Intent.ACTION_DIAL,
+						    Uri.parse("tel:01234567##"));
+
+						startActivity(intent);*/
+
+					showDialog("Selected Tel");
+					intent = new Intent(
+						    Intent.ACTION_CALL,
+						    Uri.parse("tel:01234567##"));
+
+						startActivity(intent);
 					return true;
 				case 10:
 					showDialog("Selected sub1");
@@ -98,9 +122,7 @@ public class OptionMenuSampleActivity extends Activity {
 		Intent intent = new Intent();// インテントのインスタンス
 		intent.setAction("android.media.action.IMAGE_CAPTURE");// インテントにアクションをセットする
 		startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);// カメラアプリ起動する
-
 	}
-
 
 		// showDialogメソッド(ダイアログ表示)
 		private void showDialog(String text) {
@@ -115,6 +137,35 @@ public class OptionMenuSampleActivity extends Activity {
 			});
 			dialog.create();
 			dialog.show();
+		}
+
+		@Override
+		public void onLocationChanged(Location location) {
+			double lat = location.getLatitude(); //latitude
+			double lon = location.getLongitude(); //longitude
+			String uriString = "geo:" + lat + "," + lon;
+
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriString));
+			startActivity(intent);
+
+		}
+
+		@Override
+		public void onProviderDisabled(String arg0) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+
+		@Override
+		public void onProviderEnabled(String arg0) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+
+		@Override
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO 自動生成されたメソッド・スタブ
+
 		}
 
 
